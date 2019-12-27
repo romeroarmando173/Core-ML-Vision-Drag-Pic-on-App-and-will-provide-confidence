eb6fc1de78d4920e55ce1205a724466ad8d1419f
Armando Romero <romeroarmando173@gmail.com>
2019-12-26 19:23:15 -0800

    Initial Commit

---
+++ CoreMLVisionMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLVisionMe
+//
+//  Created by Milhouse Tattoos on 12/26/19.
+//  Copyright © 2019 www.AlfaData.tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
